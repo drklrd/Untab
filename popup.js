ac41af853b84d416819edadd9afd36c72f0e4227
drklrd <srvbhattarai@gmail.com>
2017-02-05 23:06:32 +0545

    fix issue - multiple links saved at once

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 var urls = [];
 
 
-var badgeCounter = (function counter(){
+var badgeCounter = (function counter() {
 	chrome.storage.local.get('links', (result) => {
-		document.getElementById("badge").innerHTML = result && result.links ? Object.keys(result.links).length  : 0;
+		document.getElementById("badge").innerHTML = result && result.links ? Object.keys(result.links).length : 0;
 	});
 	return counter;
 })();
@@ -14,32 +14,46 @@ var saveNewLink = (links) => {
 	})
 }
 
+function alreadyExists(links,tag){
+	return links[tag] ? true : false;
+
+}
 
 
 var saveLinks = (tag, urls) => {
 
 	chrome.storage.local.get('links', (result) => {
-
+		
 		if (result && result.links) {
-			var obj = {};
-			obj[tag] = urls;
-			var links = Object.assign(result.links, obj);
-			saveNewLink(links);
+			if(!alreadyExists(result.links,tag)){
+				var obj = {};
+				obj[tag] = urls;
+				var links = Object.assign(result.links, obj);
+				saveNewLink(links);
+
+			}else{
+				document.getElementById("response").innerHTML = "This tag has already been used !";
+				return;
+			}
 
 		} else {
 			var links = {};
 			links[tag] = urls;
 			saveNewLink(links);
+			console.log('saveve',links)
 		}
 
 		document.getElementById("response").innerHTML = "Successfully untabbed !";
+		document.getElementById('search_query').value = "";
 		badgeCounter();
 
 	})
 }
 
 var opentabs = (urls) => {
+
 	if (urls && urls.length) {
+		console.log('URKKKS',urls)
 		urls.forEach(function(url) {
 			chrome.tabs.create({
 				url: url
@@ -51,8 +65,8 @@ var opentabs = (urls) => {
 document.addEventListener('DOMContentLoaded', () => {
 
 	document.getElementById('search_query').focus();
-	
-	function invokeUntab(){
+
+	function invokeUntab() {
 
 		chrome.windows.getAll({
 			populate: true
@@ -70,18 +84,18 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('save_links').addEventListener('click', () => {
 
 		invokeUntab();
-		
+
 	});
 
 
 
 	document.getElementById('view_untabs').addEventListener('click', () => {
-		chrome.storage.local.get('links', (result) => {
 
+		chrome.storage.local.get('links', (result) => {
 			if (result && result.links) {
 				var html = "";
 				for (var link in result.links) {
-					html = html + "<div class='row untab'>" +   link ;
+					html = html + "<div class='row untab'>" + link;
 					html = html + "<div class='pull-right'> <button class='btn btn-warning' id='untab_id" + link + "' > Untab </button> </div>  </div>";
 					document.getElementById("untabbed_links").innerHTML = html;
 					document.getElementById('untab_id' + link).addEventListener('click', () => {
@@ -89,13 +103,9 @@ document.addEventListener('DOMContentLoaded', () => {
 					});
 				}
 
-				// document.getElementById("favorites").innerHTML = result.favorites.join("<br>");
-
-
 			} else {
 				document.getElementById("untabbed_links").innerHTML = "No untabs found";
 			}
-
 
 		})
 	});
@@ -103,7 +113,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	document.getElementsByName('searchForm')[0].onsubmit = function(evt) {
 		evt.preventDefault();
-		invokeUntab();
 	}
 
 });
