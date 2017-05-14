3ca429184a02bf5323cad3c35b9420132ef381b2
drklrd <srvbhattarai@gmail.com>
2017-05-14 17:14:18 +0545

    add basic styling

--- import.js
+++ import.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	function onReaderLoad(event) {
 		try {
 			var importObj = JSON.parse(event.target.result);
+			if(!(importObj && importObj.links)) throw "Not valid JSON";
 			chrome.storage.local.get('links', (result) => {
 				if (result && result.links) {
 					var links = Object.assign(result.links, importObj.links);
